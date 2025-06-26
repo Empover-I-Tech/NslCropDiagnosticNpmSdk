1cea092b33307be85e4751c960ab10478bc75192
kiran.t <kiran.t@empover.com>
2025-06-26 19:54:51 +0530

    imports missing added done

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import LoaderScreen from "./src/classes/LoaderScreen"
 import CropDiagnosticsScreen from "./src/classes/CropDiagnostics/CropDiagnosticsScreen"
+import CropDesiesDetection from "./src/classes/CropDiagnostics/CropDesiesDetection"
+import WeatherScreen from "./src/classes/Weather/WeatherScreen"
+import Remedyrecommendation from "./src/classes/Weather/Remedyrecommendation"
+import Location from "./src/components/Location"
 
-export {LoaderScreen, CropDiagnosticsScreen}
+export { LoaderScreen, CropDiagnosticsScreen, CropDesiesDetection, WeatherScreen, Remedyrecommendation, Location }
